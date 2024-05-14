240b19d223c78038a62703a067e0d34f54589ed7
jules Minguet <jul.minguet@gmail.com>
2024-05-14 16:54:02 +0200

    took off deprecated code

--- HrothCore/src/HrothCore/Renderer/VertexArray.hpp
+++ HrothCore/src/HrothCore/Renderer/VertexArray.hpp
@@ -22,9 +22,6 @@ namespace HrothCore
             uint32_t GetVerticesCount() const { return m_VerticesCount; }
 
         private:
-            void GenerateVertexArray(std::vector<Vertex> vertices);
-            void GenerateVertexArrayIndexed(std::vector<Vertex> vertices, std::vector<uint32_t> indices);
-
             uint32_t m_VaoID;
             Buffer<Vertex> m_Vbo;
             Buffer<uint32_t> m_Ibo;
